e45cb2a03944d3409e3b0ab9b09d1a671cf48a41
Jemuel Balangcod <jemuelb@gmail.com>
2018-12-26 21:40:00 -0800

    Ch2 start

---
+++ KR_Ch2/ex2_1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <limits.h>
+
+main() {
+    printf("Maximum value for int: %d\n", INT_MAX);
+    printf("Minimum value for int: %d\n", INT_MIN);
+
+    printf("Maximum value for short: %d\n", SHRT_MAX);
+    printf("Minimum value for short: %d\n", SHRT_MIN);
+
+    printf("Maximum value for long: %d\n", LONG_MAX);
+    printf("Minimum value for long: %d\n", LONG_MIN);
+
+    printf("Maximum value for char: %d\n", UCHAR_MAX);
+    // printf("Minimum value for char: %d\n", );
+
+    return 0;
+}
