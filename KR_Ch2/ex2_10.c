7d76cb2e8ec2ab45e69271d465fc63b35e6d39e4
Jemuel Balangcod <jemuelb@gmail.com>
2018-12-28 22:53:13 -0800

    Chapter 2 complete

---
+++ KR_Ch2/ex2_10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+main() {
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        putchar((c >= 'A' && c <= 'Z') ? c+32 : c);
+        // putchar('\n');
+    }
+    return 0;
+}
