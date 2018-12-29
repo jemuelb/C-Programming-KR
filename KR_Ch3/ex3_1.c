c62b63902e94eb25dd3c31c074dcf3971caff40c
Jemuel Balangcod <jemuelb@gmail.com>
2018-12-29 14:00:38 -0800

    ex3_1 complete

--- KR_Ch3/ex3_1.c
+++ KR_Ch3/ex3_1.c
@@ -20,15 +20,17 @@ int binsearch(int x, int v[], int n) {
 
     low = 0;
     high = n - 1;
-    while (low <= high) {
-        mid = (low + high) / 2;
+    mid = (low + high) / 2;
+    while (low <= high && x != v[mid]) {
         if (x < v[mid])
             high = mid - 1;
-        else if (x > v[mid])
-            low = mid + 1;
         else
-            return mid;
+            low = mid + 1;
+        mid = (low + high) / 2;
     }
-    return -1;
+    if (x == v[mid])
+        return mid;
+    else
+        return -1;
 
 }
