e1d7e19fa2cdaeecf141ae78a8e2cab9a31f8249
Jemuel Balangcod <jemuelb@gmail.com>
2019-01-11 23:44:44 -0800

    ex4_4 complete

---
+++ KR_Ch4/ex4_4.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+
+int getop(char []);
+void push(double);
+double pop(void);
+void clear();
+
+main() {
+    int type;
+    double op1;
+    double op2;
+    char s[MAXOP];
+
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+            case NUMBER:
+                push(atof(s));
+                break;
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                op2 = pop();
+                push(pop() - op2);
+                break;
+            case '/':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(pop() / op2);
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case '%':
+                op2 = pop();
+                if (op2 != 0.0)
+                    push(fmod(pop(), op2));
+                else
+                    printf("error: zero divisor\n");
+                break;
+            case 'p': // print last element
+                op2 = pop();
+                printf("top element is: %g\n", op2);
+                push(op2);
+                break;
+            case 'c': // clear the stack
+                clear();
+                break;
+            case 'r': // reverse last two elements
+                op1 = pop();
+                op2 = pop();
+                push(op1);
+                push(op2);
+                break;
+            case 'd':
+                op2 = pop();
+                push(op2);
+                push(op2);
+                break;
+            case '\n':
+                printf("\t%.8g\n", pop());
+                break;
+            default:
+                printf("error: unknown command %s\n", s);
+                break;
+        }
+    }
+    return 0;
+}
+
+
+#define MAXVAL 100
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f) {
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("error: stack full, can't push %g\n", f);
+}
+
+double pop(void) {
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+int getop(char s[]) {
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && !isalpha(c) && c != '.' && c != '-')
+        return c;
+    i = 0;
+    if (isalpha(c)) {
+        getch(); // eat newline char
+        return c;
+    }
+    if (c == '-') {
+        if (isdigit(c = getch()) || c == '.')
+            s[++i] = c;
+        else {
+            ungetch(c);
+            return '-';
+        }
+    }
+    if (isdigit(c))
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.')
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void) {
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) {
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
+
+void clear() {
+    sp = 0;
+    printf("stack has been cleared\n");
+}
