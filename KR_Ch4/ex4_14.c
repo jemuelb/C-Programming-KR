dd617570086103f299075fd8a0d603a5fa517ee6
Jemuel Balangcod <jemuelb@gmail.com>
2019-01-12 17:06:32 -0800

    Chapter 4 complete

---
+++ KR_Ch4/ex4_14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define swap(t, x, y) { t _z;   \
+                        _z = y; \
+                        y = x;  \
+                        x = _z; }
+
+main() {
+
+    return 0;
+}
